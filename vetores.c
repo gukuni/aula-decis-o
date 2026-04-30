f8f42bc292379fbd1b988f32ccbfb248fee1c250
gukuni <guhkunitaki@gmail.com>
2026-04-30 10:19:55 -0300

    aula de decisões

---
+++ vetores.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void exibir_valor(int vetor[], int n){
+    for (int i = 0; i < n ; i++){
+        int valor = vetor[i];
+        printf("O indice do valor e: %d\nO valor do vetor e:%d\n\n", i, valor);
+
+    }
+}
+int main(){
+    int quantidade;
+    printf("Digite quantidade de notas que gostaria: ");
+    scanf("%d", &quantidade);
+    float notas[quantidade];
+    float soma = 0;
+
+    for(int i =0, i < quantidade,i++ ){
+        printf("Digite a nota: ");
+        scanf("%d", &soma);
+    }
+
+
+
+
+
+    return 0;
+}
