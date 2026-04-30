bc75f784da49930dae2f127eba826840ba324285
gukuni <guhkunitaki@gmail.com>
2026-04-30 10:46:05 -0300

    finalização do projeto

--- vetores.c
+++ vetores.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void exibir_valor(int vetor[], int n){
+void exibir_valor(float vetor[], int n){
     for (int i = 0; i < n ; i++){
         int valor = vetor[i];
         printf("O indice do valor e: %d\nO valor do vetor e:%d\n\n", i, valor);
@@ -10,13 +10,18 @@ int main(){
     int quantidade;
     printf("Digite quantidade de notas que gostaria: ");
     scanf("%d", &quantidade);
-    float notas[quantidade];
+    float notas[400];
     float soma = 0;
 
-    for(int i =0, i < quantidade,i++ ){
+    for(int i =0; i<quantidade; i++ ){
         printf("Digite a nota: ");
-        scanf("%d", &soma);
+        scanf("%f", &notas[i]);
+        soma += notas[i];
     }
+    float media = soma / quantidade;
+    exibir_valor(notas, quantidade);
+    printf("Essa e a sua media: %f\n", media);
+
 
 
 
